3062b2564ff1428da53b639a2e174649e575c421
Evgeny <66548875+AfanasevEvgeny@users.noreply.github.com>
2020-11-04 23:47:37 +0300

    Create ex2.c

---
+++ week12_/ex2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char const *argv[])
+{
+    char buf[1000];
+    int l = 0;
+    while (read(STDIN_FILENO, &buf[l++], 1) > 0);
+    printf("%s", buf);
+    int file;
+    if (argc == 2)
+    {
+        fclose(fopen("ex2.txt", "wb"));
+        file = open(argv[1], O_WRONLY);
+    }
+    else if (argc == 3)
+    {
+        fclose(fopen("ex2.txt", "ab+"));
+        file = open(argv[2], O_WRONLY | O_APPEND);
+    }
+
+    write(file, buf, l - 1);
+    close(file);
+
+    return 0;
+}
